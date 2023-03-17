f51e3e4919b3b4e0ff01526e67fec1a7cdfd9efc
Vinod Kumar Kayartaya <vinod@vinod.co>
2023-03-17 13:19:34 +0530

    🇮🇳

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div className='alert alert-primary'>
+            <div className='container'>
+                <h1>Todo app</h1>
+                <p>Powered with React and Redux</p>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
